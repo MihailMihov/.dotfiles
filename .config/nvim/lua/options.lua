54bd39fe604d1dfd7c15a28f83547a8a51c1c5dc
Mihail Mihov <mihovmihailp@gmail.com>
2024-04-19 01:11:10 +0300

    Update

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -22,3 +22,13 @@ vim.opt.inccommand = 'split'
 vim.opt.cursorline = true
 
 vim.opt.scrolloff = 10
+
+vim.opt.hlsearch = true
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
